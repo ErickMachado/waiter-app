3357299a74278255df6953bcbf40befd931baf80
¨Erick <erick.gsmachado@gmail.com>
2022-11-20 11:21:09 -0300

    add static path to images

--- api/src/main/app/app.ts
+++ api/src/main/app/app.ts
@@ -1,9 +1,14 @@
+import path from 'node:path';
 import { router } from '@/main/app/routes';
 import express from 'express';
 
 const app = express();
 
 app.use(express.json());
+app.use(
+  '/uploads',
+  express.static(path.resolve(__dirname, '..', '..', '..', 'uploads'))
+);
 app.use(router);
 
 export { app };
