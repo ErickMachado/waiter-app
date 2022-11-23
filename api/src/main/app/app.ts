2efa5f72a67284f051f7ab21408429989ebbae18
¨Erick <erick.gsmachado@gmail.com>
2022-11-23 20:51:14 -0300

    update api doc

--- api/src/main/app/app.ts
+++ api/src/main/app/app.ts
@@ -1,9 +1,11 @@
 import path from 'node:path';
 import { router } from '@/main/app/routes';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(
   '/uploads',
